c0ca5dd20a4c9892b8e0e18c68c7bc958f62b55b
Marco Segalla <marco.segalla@sorteonline.com.br>
2023-01-18 10:05:23 -0300

    Criando estrutura de temas e pastas

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ThemeProvider } from 'styled-components'
 import App from './App'
-import './index.css'
+import { GlobalStyles } from './styles/globalStyles'
+import theme from './styles/theme'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
