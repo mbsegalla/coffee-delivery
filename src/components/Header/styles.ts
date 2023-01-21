a670827c4b1a39ed00f9471deb5488701b80d7d1
Marco Segalla <marco.segalla@sorteonline.com.br>
2023-01-21 09:40:35 -0300

    Criando informação sobre quantidade de items no carrinho em tela

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -37,3 +37,20 @@ export const IconButton = styled.button`
     transition: ease-in-out 0.2s;
   }
 `
+
+export const TotalItems = styled.div`
+  position: absolute;
+  right: 10;
+  color: ${(props) => props.theme.white};
+  font-weight: 700;
+  font-size: 0.75rem;
+  background-color: ${(props) => props.theme['yellow-dark']};
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  top: 25px;
+  right: 155px;
+`
